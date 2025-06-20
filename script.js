25297e117a1661adc3d13f31e453fdfa61cc8c06
QasimAshraf-afk <dha147x@gmail.com>
2025-06-20 10:31:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add interactivity here, e.g., change intro text on click
+document.querySelector('.intro p').addEventListener('click', () => {
+    alert('Thanks for checking out my portfolio!');
+  });
+  
